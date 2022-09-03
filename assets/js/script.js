c071a2b7365174f3574699b7d99a562b6489c0b2
sheyda99 <sheydasultan.99@gmail.com>
2022-09-03 11:27:49 +0400

    fixing homepage responsibility

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,25 @@
+$(document).ready(function(){
+    $("#mobile-menu-trigger").click(function(){
+        $("body").toggleClass("mobile-menu-open");
+        $("#mobile-menu-trigger").toggleClass("is-clicked");
+    });
+});
+
+
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
     if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
         document.getElementById('header').classList.add("minimize");
+        document.getElementById('mobile-menu-trigger').classList.add("minimize");
     } else {
         document.getElementById('header').classList.remove("minimize");
+        document.getElementById('mobile-menu-trigger').classList.remove("minimize");
+
     }
 
-    // if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    //     document.getElementsByClassName('back-to-top')[0].classList.add("is-visible");
-    // } else {
-    //     document.getElementsByClassName('back-to-top')[0].classList.remove("is-visible");
-    // }
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementsByClassName('back-to-top')[0].classList.add("is-visible");
+    } else {
+        document.getElementsByClassName('back-to-top')[0].classList.remove("is-visible");
+    }
 }
