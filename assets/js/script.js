78094b7a1f86eae7ed5cd9140470e3c50d457c45
sheyda99 <sheydasultan.99@gmail.com>
2022-09-05 20:57:58 +0400

    lst version of frontend

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,14 @@ $(document).ready(function(){
 
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
-    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-        document.getElementById('header').classList.add("minimize");
-        document.getElementById('mobile-menu-trigger').classList.add("minimize");
-    } else {
-        document.getElementById('header').classList.remove("minimize");
-        document.getElementById('mobile-menu-trigger').classList.remove("minimize");
+    // if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    //     document.getElementById('header').classList.add("minimize");
+    //     document.getElementById('mobile-menu-trigger').classList.add("minimize");
+    // } else {
+    //     document.getElementById('header').classList.remove("minimize");
+    //     document.getElementById('mobile-menu-trigger').classList.remove("minimize");
 
-    }
+    // }
 
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
         document.getElementsByClassName('back-to-top')[0].classList.add("is-visible");
