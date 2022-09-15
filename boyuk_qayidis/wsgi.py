75734f61c526cbe7a1ee2bb0a1410b3451daff1a
sheyda99 <sheydasultan.99@gmail.com>
2022-09-16 00:57:17 +0400

    back to the previous version

--- boyuk_qayidis/wsgi.py
+++ boyuk_qayidis/wsgi.py
@@ -14,6 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boyuk_qayidis.settings')
 
 application = get_wsgi_application()
-# vercel codes
-app = application
-# vercel codes ended
--- build_files.sh
+++
@@ -1,5 +0,0 @@
-# build_files.sh
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
