1caddb8e1bab50a168bafdef6ae60c4d8d8be0de
amurad77 <amurad7799@gmail.com>
2022-10-06 01:01:08 +0400

    allowed hosts

--- boyuk_qayidis/settings.py
+++ boyuk_qayidis/settings.py
@@ -156,17 +156,21 @@ USE_L10N = True
 USE_TZ = True
 
 
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-if PROD:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
-else:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static")
-    ]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# if PROD:
+#     STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# else:
+#     STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, "static")
+#     ]
 
 
 # Default primary key field type
