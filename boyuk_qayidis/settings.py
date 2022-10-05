564be1d3f789af73c27ba8425771fae268e60604
amurad77 <amurad7799@gmail.com>
2022-10-06 00:28:03 +0400

    allowed hosts

--- boyuk_qayidis/settings.py
+++ boyuk_qayidis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xzt0)1fdff1j8-mc2z^=p(or1^wdzl_5#y4i=gpbv8mqd)1abo
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.environ.get('DEBUG') else True
-PROD = not DEBUG
+# PROD = not DEBUG
 
 
 ALLOWED_HOSTS = ['*']
