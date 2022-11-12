ba30fade438e1039e5f88d2a45b220bce8859197
amurad77 <amurad7799@gmail.com>
2022-11-13 00:30:37 +0400

    fix

--- boyuk_qayidis/settings.py
+++ boyuk_qayidis/settings.py
@@ -163,13 +163,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_URL = './static/'
+STATIC_URL = '/static/'
 
 if PROD:
-    STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 else:
     STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "/static")
+        os.path.join(BASE_DIR, "static")
     ]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
