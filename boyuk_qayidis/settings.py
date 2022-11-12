ce3d8ac9865f2b4b041073bdf33631bdbda21f31
amurad77 <amurad7799@gmail.com>
2022-11-13 00:31:59 +0400

    fix

--- boyuk_qayidis/settings.py
+++ boyuk_qayidis/settings.py
@@ -166,10 +166,10 @@ USE_TZ = True
 STATIC_URL = './static/'
 
 if PROD:
-    STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+    STATIC_ROOT = os.path.join(BASE_DIR, "./static/")
 else:
     STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "/static/")
+        os.path.join(BASE_DIR, "./static/")
     ]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
