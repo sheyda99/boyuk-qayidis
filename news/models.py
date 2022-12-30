c05bb708a0b8c6f1730460d33381ae09a02b846a
Sheyda Sultan <sheydasultan.99@gmail.com>
2022-12-30 16:33:06 +0400

    Update models.py

--- news/models.py
+++ news/models.py
@@ -24,7 +24,7 @@ class News(models.Model):
     description5 = models.TextField('Məzmun4', max_length = 2000, null=True, blank=True)
     pdf_title = models.CharField('Pdf faylının adı', max_length = 256, null=True, blank=True)
     pdf = models.FileField('Pdf faylı', upload_to='media/pdf', null=True, blank=True)
-    slug = models.SlugField('Slug', max_length = 110, unique = True, editable = False)
+    slug = models.SlugField('Slug', max_length = 256, unique = True, editable = False)
 
     # moderations
     created_at = models.DateTimeField(auto_now_add=True)
